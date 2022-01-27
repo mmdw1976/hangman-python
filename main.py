c60dfaf454f66c21a7b2fe4ab10111b545878fdb
Mario <mmdw1976@gmail.com>
2022-01-27 23:01:55 +0100

    Setup comments about game flow

---
+++ main.py
@@ -0,0 +1,25 @@
+# import random module
+
+# import words
+
+# setup random word
+
+# setup lives
+
+# setup display
+
+# for each letter in word create "_" in display
+
+# begin game loop
+    # show display to user
+
+    # ask user choose a letter
+
+    # check if user choice match word
+    # # if choice match word loop over word to find letter
+    # ## when found replace the  "_" in display at the correct position
+
+    # check if "_" not in chosen word if so you win game
+
+    # check if player have turns left if not game over
+    # # stop loop
